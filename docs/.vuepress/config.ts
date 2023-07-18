242ce601849a18e9bdb9d1b615cddd3ef0048a43
Cwd295645351 <295645351@qq.com>
2023-07-18 11:14:27 +0800

    feat: 完善网站链接

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,41 +1,34 @@
-import { defineConfig } from "vuepress/config"
+import { defineConfig } from 'vuepress/config'
 
-export default defineConfig(ctx => ({
-	title: "前端小站",
-	/**
-	 * Type is `DefaultThemeConfig`
-	 */
-	base: '/docs/',
-	themeConfig: {
-		repo: "vuejs/vuepress",
-		editLinks: true,
+export default defineConfig((ctx) => ({
+  title: '前端小站',
+  /**
+   * Type is `DefaultThemeConfig`
+   */
 
-		search: true,
-		searchMaxSuggestions: 10,
+  base: '/docs/',
+  themeConfig: {
+    repo: 'https://github.com/Cwd295645351/docs',
+    repoLabel: '查看源码',
+    editLinks: true,
+    docsDir: 'docs',
+    docsBranch: 'main',
 
-		// 默认为 "Edit this page"
-		editLinkText: "帮助我们改善此页面！",
-		nav: [
-			{ text: "设计模式", link: "/design/" }, // 内部链接 以docs为根目录
-		],
-		sidebar: {
-			"/design/": [
-				"",
-				"adapter",
-				"combination",
-				"decorator",
-				"chain-of-responsibility",
-				"publish-subcribe", 
-				"proxy",
-				"single",
-				"status",
-				"strategy"
-			],
-			"/foo/": ["", "one", "two"],
-			// fallback
-			"/": ["" /* / */]
-		},
-		sidebarDepth: 2,
-		lastUpdated: 'Last Updated', // string | boolean
-	}
+    search: true,
+    searchMaxSuggestions: 10,
+
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面！',
+    nav: [
+      { text: '设计模式', link: '/design/' }, // 内部链接 以docs为根目录
+    ],
+    smoothScroll: true,
+    sidebar: {
+      '/design/': ['', 'adapter', 'combination', 'decorator', 'chain-of-responsibility', 'publish-subcribe', 'proxy', 'single', 'status', 'strategy'],
+      '/foo/': ['', 'one', 'two'],
+    },
+
+    sidebarDepth: 2,
+    lastUpdated: 'Last Updated', // string | boolean
+  },
 }))
